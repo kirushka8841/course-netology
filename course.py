492b7bf43080b175cd38e6ef0995d88130d5177c
Kirushka88 <kirushka8841@yandex.ru>
2023-03-31 22:20:45 +0600

    files changed

--- course.py
+++ course.py
@@ -1,56 +1,116 @@
-import time
 import requests
-from pprint import pprint
 import json
-import os
-import datetime
 from tqdm import tqdm
-
-# with open('token.txt', 'r') as file_object:
-#     token = file_object.read().strip()
-os.chdir(r'C:\Users\Елена\OneDrive\Рабочий стол\курсовая')
-token = open('token.txt')
-token_ya = open('token_ya.txt')
-
+import datetime as dt
+import time
+import configparser
+from settings import token_ya, token_vk
 
 class VK:
     
-    def __init__(self, token, user_ids, version=5.131, count=5):
-       self.count = count
-       self.user_ids = user_ids
-       self.version = version
-       self.token = token
     
-    def users_info(self, user_ids):
-        URL = 'https://api.vk.com/method/users.get'
-        user_params = {
-                'user_ids': user_ids 
-            }
-        res = requests.get(URL, params=user_params).json()
-        return res
+    def __init__(self, token, version, user_ids, count = 5):
+        self.user_ids = user_ids
+        self.count = count
+        self.params = {
+            'access_token': token,
+            'v': version
+        }
 
-    def get_photos():
-        # user_info = self.users_info(self.user_ids)
-        # name = f"{user_info['response'][0]['first_name']} {user_info['response'][0]['last_name']}"
+
+    def get_user(self, user_ids):
+        get_user_url = 'https://api.vk.com/method/users.get'
+        get_user_params = {
+            'user_ids': user_ids
+        }
+        user_info = requests.get(get_user_url, params={**self.params, **get_user_params}).json()
+        return user_info
+
+    
+    def get_photo(self):
         url = 'https://api.vk.com/method/photos.get'
         params = {
-            'owner_id': input('Введите id пользователя\n'),
+            'owner_id': self.user_ids,
             'album_id': 'profile',
             'photo_sizes': '1',
             'extended': '1',
-            'access_token': token,
-            'v': 5.131,
-            'count': 5
+            'count': self.count
         }
-        res = requests.get(url, params = params)
-        res = res.json()
-        if res['response']['count'] == 0:
-            print(f'У пользователя нет фото')
+        res = requests.get(url, params={**self.params, **params}).json()
+        return res
+
+
+class YandexDisk:
+    
+    
+    def __init__(self, token, list_photos):
+        self.token = token
+        self.list_photos = list_photos
+
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+    
+
+    def _get_upload_link(self, path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        params = {"path": path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=self.get_headers, params=params)
+        return response.json()['href']
+    
+
+    def create_folder(self):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        path = input('Введите название новой папки\n')
+        params = {'path': path}
+        response = requests.put(upload_url, headers=self.get_headers(), params=params)
+        if response.status_code == 409:
+            check_folder_exists = input('Такая папка уже существует, продолжить загрузку в эту папку? Y/N\n').lower()
+            if check_folder_exists == 'N':
+                exit()
+        return path
+    
+
+    def upload(self):
+        list_photos_for_json = []
+        path = self.create_folder()
+        for photo in tqdm(self.list_photos):
+            file_name = photo['file_name']
+            size = photo['size']
+            link_for_upload = photo['max_size_link']
+            upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+            params = {'path': f'/{path}/' + file_name + '.jpg', 'url': link_for_upload}
+            response = requests.post(upload_url, params=params, headers=self.get_headers())
+            if response.status_code != 202:
+                print('Ошибка загрузки')
+            list_photos_for_json.append(
+                {
+                    'file_name': file_name,
+                    'size': size
+                }
+            )
+        with open('Result.json', 'w') as file:
+            json.dump(list_photos_for_json, file)
+        print('Загружено')
+    
+
+class Main:
+    def save_photo(user_ids, count):
+    # config = configparser.ConfigParser()
+    # config.read('settings.ini')
+    # token_vk = config['api']['token_vk']
+    # token_ya = config['api']['token_ya']
+        res = VK(token_vk, 5.131, user_ids, count).get_photo()
         list_photos = []
         list_likes = []
+        time.sleep(0.3)
+        date_photo = dt.date.today().strftime('%d.%m.%Y')
+
         for foto in res['response']['items']:
-            time.sleep(0.3)
-            date_photo = datetime.datetime.fromtimestamp(foto['date']).strftime('%Y-%m-%d')
+            
             if foto['likes']['count'] in list_likes:
                 list_photos.append(
                     {
@@ -67,69 +127,12 @@ class VK:
                         'max_size_link': foto['sizes'][-1]['url']
                     }
                 )
-            list_likes.append(foto['likes']['count'])
-        return list_photos
-
-
-class YandexDisk:
-
-    def __init__(self, list_photos):
-        self.token = token
-        self.list_photos = list_photos
-
-    def get_headers(self):
-        return {
-            'Content-Type': 'application/json',
-            'Authorization': 'OAuth {}'.format(self.token)
-        }
-
-    def _get_upload_link(self, path):
-        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-        params = {"path": path, "overwrite": "true"}
-        response = requests.get(upload_url, headers=self.get_headers, params=params)
-        return response.json()['href']
+                list_likes.append(foto['likes']['count'])
+            return list_photos
     
-    def create_folder(self):
-        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-        path = input('Введите название новой папки\n')
-        params = {'path': path}
-        response = requests.put(upload_url, headers=self.get_headers(), params=params)
-        if response.status_code == 409:
-            check_folder_exists = input('Такая папка уже существует, продолжить загрузку в эту папку? Y/N\n').lower()
-            if check_folder_exists == 'n':
-                exit()
-        return path
-
-    def upload(self):
-        list_photos_for_json = []
-        path = self.create_folder()
-        for photo in tqdm(self.list_photos):
-            file_name = photo['file_name']
-            size = photo['size']
-            link_for_upload = photo['max_size_link']
-            upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-            params = {'path': f'/{path}/' + file_name + '.jpg', 'url': link_for_upload}
-            response = requests.post(upload_url, params=params, headers=self.get_headers())
-
-            if response.status_code != 202:
-                print('Ошибка загрузки')
-            list_photos_for_json.append(
-                {
-                    'file_name': file_name,
-                    'size': size
-                }
-            )
-        with open('Result.json', 'w') as file:
-            json.dump(list_photos_for_json, file)
-        print('Загружено')
 
 if __name__ == '__main__':
+    user_id = input('Введите id пользователя\n')
     count_photos = input('Введите количество скачиваемых фото\n')
-    # YandexDisk(token_ya, get_photos(user_id, count_photos).upload())
-    # YandexDisk(VK.get_photos(token)).upload()
-    # ya.upload(VK.get_photos(token))
-    uploader_ya = YandexDisk(token_ya)
-    uploader_vk = VK.get_photos()
-    uploader_ya.upload(uploader_vk)
-    
+    YandexDisk.upload(token_ya, Main.save_photo(user_id, count_photos))
 # '473860275'
